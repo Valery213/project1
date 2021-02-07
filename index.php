fb274f1d38e265059c0ef410ffc417fda79d58f9
root <q8x8l@yandex.ru>
2021-02-07 20:08:58 +0300

    Закрыли мета тег вьюпорт

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 	echo '</div>';
 	include 'Блоки/Ноги.php';
 ?>
-<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
 <style>
 	* {
 		margin: 0;
